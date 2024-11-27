3d9108ab9e105f0fdbf1617204c78185628f9cfe
Hayden Donald <haydendonald@gmail.com>
2024-11-27 21:25:31 +1100

    Scene updates
    Add alwaysStage option
    Add transition time to unstage

--- types/scene.ts
+++ types/scene.ts
@@ -9,15 +9,16 @@ export class SceneReference {
 }
 
 export interface Scene {
-    id: number;
-    name: string;
-    description: string;
-    attributes: SceneAttributes;
+    id: number; //The unique identifier of the scene
+    name: string; //The name of the scene
+    description: string; //The description of the scene
+    attributes: SceneAttributes; //The attributes of the scene
 }
 
 export interface SceneAttributes {
-    priority?: number;
-    globalTransitionTime?: number;
-    globalBrightness?: number;
-    states: LightState[];
+    priority?: number; //The priority of the scene, higher numbers will mean this scene replaces others with lower priority
+    globalTransitionMs?: number; //Force all lights to transition at this speed
+    globalBrightnessPercent?: number; //Scale the brightness of lights by this amount
+    alwaysStage?: boolean; //Always stage this scene, if true, it cannot be unstaged
+    states: LightState[]; //The states of the lights in this scene
 }
